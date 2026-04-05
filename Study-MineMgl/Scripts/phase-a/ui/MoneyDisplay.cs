f2f852445b5a9081bc6826a101d46e5e65ffb5b9
an-alch3mist <kaeya000@gmail.com>
2026-04-05 17:51:25 +0600

    MineMGL 3

--- Study-MineMgl/Scripts/phase-a/ui/MoneyDisplay.cs
+++ Study-MineMgl/Scripts/phase-a/ui/MoneyDisplay.cs
@@ -12,8 +12,32 @@ using SPACE_UTIL;
 
 namespace SPACE_MineMGL
 {
+	/// <summary>
+	/// sunscribes to game event for decoupled updates
+	/// </summary>
 	public class MoneyDisplay : MonoBehaviour
 	{
-		
+		[SerializeField] TextMeshProUGUI _moneyText;
+
+		#region private API
+		void HandleMoneyChanged(float newAmount)
+		{
+			if (Singleton<EconomyManager>.Ins != null)
+				this._moneyText.text = Singleton<EconomyManager>.Ins.GetMoney().formatMoney();
+			else
+				this._moneyText.text = "No Economy Manager Ins".colorTag("red");
+		}
+		#endregion
+
+		#region Unity Life Cycle
+		private void OnEnable()
+		{
+			
+		}
+		private void OnDisable()
+		{
+			
+		}
+		#endregion
 	}
 }
--- Study-MineMgl/Scripts/phase-a/ui/UIManager.cs
+++ Study-MineMgl/Scripts/phase-a/ui/UIManager.cs
@@ -12,13 +12,43 @@ using SPACE_UTIL;
 
 namespace SPACE_MineMGL
 {
+	/// <summary>
+	/// singleton with global UI state: menu's that are open, cursor visibility etc
+	/// </summary>
 	public class UIManager : Singleton<UIManager>
 	{
-		
+		[SerializeField] ShopUI shopUI;
+		[SerializeField] InteractionWheelUI interactionWheelUI;
+
+		[SerializeField] GameObject _hudObj;
+		[SerializeField] GameObject _backgroundBlur;
 
 		public bool IsInAnyMenu()
 		{
+			if (this.shopUI.gameObject.activeSelf == true)
+				return true;
+			if (interactionWheelUI.gameObject.activeSelf)
+				return true;
 			return false;
 		}
+		public bool IsInShopMenu()
+		{
+			return this.shopUI.gameObject.activeSelf;
+		}
+		public void SetBgBlur(bool enabled)
+		{
+			this._backgroundBlur.toggle(enabled);
+		}
+		public void SetHudElems(bool enabled)
+		{
+			this._hudObj.toggle(enabled);
+		}
+
+		#region Unity Life Cycle
+		private void LateUpdate()
+		{
+			this.SetBgBlur(this.IsInAnyMenu();)
+		}
+		#endregion
 	}
 }
